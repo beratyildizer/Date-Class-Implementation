9dd14fa0a2eda7e22b0e170e5386279b7e6a6f49
Berat Yıldızer <44676948+beratyildizer@users.noreply.github.com>
2023-01-10 20:23:25 +0300

    Add files via upload

---
+++ date.cpp
@@ -0,0 +1,398 @@
+#include "date.h"
+
+using namespace project;
+using namespace std;
+
+unsigned int Date::sumOfDay(const Date& x)
+{
+	unsigned int sum{};
+	for (int i = Date::yearBase; i < x.year; ++i) {
+		sum += (isleap(i) ? 366 : 365);
+	}
+	for (int i = 1; i < x.month; ++i) {
+		sum += Date::dayOfMonths[i];
+	}
+	return ((x.month > 2 && isleap (x.year)) ? sum + x.dayOfMonth + 1 : sum + x.dayOfMonth);
+}
+
+Date::Date(int d, int m, int y)
+		{
+			if (!((y < yearBase || y > 2023) || (m < 1 || m > 12) || (d < 1 || d > 31))) {
+				if (!(dayOfMonths[m] < d)) {
+					int sumOfDay{};
+					for (int i = yearBase; i < y; ++i) {
+						if (isleap(i))
+							sumOfDay += 366;
+						else sumOfDay += 365;
+					}
+					for (int i = 1; i < m; ++i)
+						sumOfDay += dayOfMonths[i];
+					if (m > 2 && isleap(y))
+						sumOfDay += 1;
+					sumOfDay += d - 1;
+					year = y;
+					month = m;
+					dayOfMonth = d;
+					day = (dayArray[sumOfDay % 7]);
+				}
+			}
+}
+
+int Date::getMonthDay()const { return dayOfMonth; } //++
+void Date::printMonthDay()const { cout << dayOfMonth << "\n"; };
+
+int Date::getMonth()const { return month; } //++
+void Date::printMonth()const { cout << month << "\n"; }
+
+int Date::getYear()const { return year; } //++
+void Date::printYear()const { cout << year << "\n"; }
+
+int Date::getYearDay()const
+{
+	int result{};
+	for (int i = 1; i < month; ++i)
+		result += dayOfMonths[i];
+	return ((month > 2 && isleap(year)) ? result + dayOfMonth + 1 : result + dayOfMonth);
+}
+
+void Date::printYearDay()const
+{
+	int sum{};
+	for (int i = 1; i < month; ++i)
+		sum += dayOfMonths[i];
+	sum += dayOfMonth;
+	if (month > 2)
+		sum += isleap(year) ? 1 : 0;
+	cout << sum;
+}
+
+Weekday Date::getWeekDay()const
+{
+	return day;
+}
+
+void Date::printWeekDay()const
+{
+	cout << dayStrings[(int)this->day];
+}
+
+Date& Date::setMonthDay(int day)
+{
+	dayOfMonth = day;
+	return *this;
+}
+
+Date& Date::setMonth(int month)
+{
+	this->month = month;
+	return *this;
+}
+
+Date& Date::setYear(int year)
+{
+	this->year = year;
+	return *this;
+}
+
+Date& Date::set(int d, int m, int y)
+{
+	if (!((y < yearBase || y > 2023) || (m < 1 || m > 12) || (d < 1 || d > 31))) {
+		if (!(dayOfMonths[m] < d)) {
+			int sumOfDay{};
+			for (int i = yearBase; i < y; ++i) {
+				if (isleap(i))
+					sumOfDay += 366;
+				else sumOfDay += 365;
+			}
+			for (int i = 1; i < m; ++i)
+				sumOfDay += dayOfMonths[i];
+			if (m > 2 && isleap(y))
+				sumOfDay += 1;
+			sumOfDay += d - 1;
+			year = y;
+			month = m;
+			dayOfMonth = d;
+			day = (dayArray[sumOfDay % 7]);
+		}
+	}
+	return *this;
+}
+
+Date Date::operator-(int day)const
+{
+	if (day < 1)
+		return *this;
+	Date temp{ *this };
+	int newDay = temp.getYearDay() - day;
+	if (newDay > 0)
+	{
+		int x = 0;
+		for(int i = 1; i <= temp.month; ++i)
+		{
+			x += temp.dayOfMonths[i];
+			if (i == 2 && isleap(temp.year))
+				++x;
+			if (x >= newDay) {
+				temp.month = i;
+				temp.dayOfMonth = newDay - (x - temp.dayOfMonths[i]);
+				break;
+			}
+		}
+	}
+	else {
+		newDay = -newDay;
+		--temp.year;
+		while (newDay > 365) {
+			--temp.year;
+			if (temp.year < 1922) {
+				return Date{};
+			}
+			newDay -= isleap(temp.year) ? 366 : 365;
+		}
+		int hand = newDay % (isleap(temp.year) ? 366 : 365);
+		int x = 0;
+		for (int i = 12; i > 0; --i) {
+			x += temp.dayOfMonths[i];
+			if (i == 2 && isleap(temp.year))
+				++x;
+			if (x >= hand) {
+				temp.month = i;
+				temp.dayOfMonth = temp.dayOfMonths[i] - (hand - (x - temp.dayOfMonths[i]));
+				break;
+				}
+			}	
+	}
+	int dayTemp = (int)temp.day - (day % 7);
+	if (dayTemp < 0)
+		temp.day = Date::dayArray[7 + dayTemp]; 
+	else
+		temp.day = Date::dayArray[dayTemp];
+	return temp;
+}
+
+Date& Date::operator+=(int day)
+{
+	if (day < 1)
+		return *this;
+	int newDay = getYearDay() + day;
+	if (newDay <= (isleap(year) ? 366 : 365)) {
+		int sum{ (getYearDay() - dayOfMonth)};
+		if (newDay > sum) {
+			for (int i = month; i < 13; ++i) {
+				sum += dayOfMonths[i];
+				month = i;
+				if (i == 2 && isleap(year))
+					++sum;
+				if (newDay <= sum) {
+					dayOfMonth = newDay - (sum - dayOfMonths[i]);
+					if (month == 2 && isleap(year))
+						++dayOfMonth;
+					break;
+				}
+
+			}
+		}
+	}
+	else {
+		newDay -= (isleap(year) ? 366 : 365);
+		++year;
+		while(newDay > (isleap(year) ? 366 : 365)){
+			++year;
+			newDay -= (isleap(year) ? 366 : 365);
+			if (year > 2023) {
+				year = 2023;
+				month = 12;
+				dayOfMonth = 31;
+				this->day = Weekday::Saturday;
+				return *this;
+			}
+		}
+		int sum{};
+		for (int i = 1; i < 13; ++i) {
+			if (newDay == sum) {
+				dayOfMonth = dayOfMonths[i - 1];
+				break;
+			}
+			sum += dayOfMonths[i];
+			month = i;
+			if (i == 2 && isleap(year))
+				++sum;
+			if (newDay < sum) {
+				dayOfMonth = newDay - (sum - dayOfMonths[i]);
+				break;
+			}
+		}
+	}
+	int dayTemp = (int)Date::day + (day % 7);
+	if (dayTemp > 6)
+		Date::day = Date::dayArray[dayTemp - 7];
+	else
+		Date::day = Date::dayArray[dayTemp];
+	return *this;
+}
+
+Date& Date::operator-=(int day)
+{
+	if (day < 1)
+		return *this;
+	int newDay = getYearDay() - day;
+	if (newDay > 0)
+	{
+		int x = 0;
+		for (int i = 1; i <= month; ++i)
+		{
+			x += dayOfMonths[i];
+			if (i == 2 && isleap(year))
+				++x;
+			if (x >= newDay) {
+				month = i;
+				dayOfMonth = newDay - (x - dayOfMonths[i]);
+				break;
+			}
+		}
+	}
+	else {
+		newDay = -newDay;
+		--year;
+		while (newDay > 365) {
+			--year;
+			if (year < 1922) {
+				year = 1922;
+				month = 1;
+				dayOfMonth = 1;
+				this->day = Weekday::Sunday;
+				return *this;
+			}
+			newDay -= isleap(year) ? 366 : 365;
+		}
+		int hand = newDay % (isleap(year) ? 366 : 365);
+		int x = 0;
+		for (int i = 12; i > 0; --i) {
+			x += dayOfMonths[i];
+			if (i == 2 && isleap(year))
+				++x;
+			if (x >= hand) {
+				month = i;
+				dayOfMonth = dayOfMonths[i] - (hand - (x - dayOfMonths[i]));
+				break;
+			}
+		}
+	}
+	int dayTemp = (int)day - (day % 7);
+	if (dayTemp < 0)
+		this->day = Date::dayArray[7 + dayTemp];
+	else
+		this->day = Date::dayArray[dayTemp];
+	return *this;
+}
+
+Date& Date::operator++()
+{
+	++dayOfMonth;
+	day = (Weekday)((day < Weekday::Saturday) ? (int)day + 1 : 0);
+	if (dayOfMonth > dayOfMonths[month]) {
+		if (!((month == 2 && isleap(year)) && (dayOfMonth <= (dayOfMonths[month] + 1)))) {
+			dayOfMonth = 1;
+			++month;
+			if (month > 12) {
+				month = 1;
+				++year;
+			}	
+		}	
+	}
+	if (year > 2023) {
+		year = 2023;
+		dayOfMonth = 1;
+		month = 1; 
+		day = Weekday::Sunday;
+	}
+
+	return *this;
+
+}
+
+Date Date::operator++(int)
+{
+	Date temp = *this;
+	++(*this);
+	return temp;
+}
+
+Date& Date::operator--()
+{
+	--dayOfMonth;
+	if (dayOfMonth < 1) {
+		--month;
+		if (!month) {
+			month = 12;
+			--year;
+			dayOfMonth = dayOfMonths[month];
+		}
+		else if (month == 2 && isleap(year)) {
+			dayOfMonth = dayOfMonths[month];
+			++dayOfMonth;
+		}else dayOfMonth = dayOfMonths[month];
+	}
+	return *this;
+}
+
+Date Date::operator--(int)
+{
+	Date  temp = *this;
+	--(*this);
+	return temp;
+}
+
+bool project::operator<=(const Date& x, const Date& y)
+{
+	return ((x < y) || (x == y));
+}
+
+bool project::operator>=(const Date& x, const Date& y)
+{
+	return !(x <= y);
+}
+
+bool project::operator>(const Date& x, const Date& y)
+{
+	return !(x < y);
+}
+
+bool project::operator!=(const Date& x, const Date& y)
+{
+	return !(x == y);
+}
+
+constexpr bool Date::isleap_(int y)
+{
+	return isleap(y);
+}
+
+int project::operator-(const Date& d1, const Date& d2)
+{
+	return Date::sumOfDay(d1) - Date::sumOfDay(d2);
+}
+
+Date project::operator+(const Date& date, int n)
+{
+	Date temp{ date };
+	return temp += n;
+}
+
+Date project::operator+(int n, const Date& date)
+{
+	Date temp{ date };
+	return temp += n;
+}
+
+Weekday& project::operator++(Weekday& x)
+{
+	return x = (Weekday)((int)(++x) % 7);
+} 
+
+Weekday project::operator++(Weekday& x, int y)
+{
+	Weekday temp = x;
+	return (++temp);
+}
+
---
+++ date.h
@@ -0,0 +1,129 @@
+#ifndef DATE_H
+#define DATE_H
+
+#include <iostream>
+#include <iosfwd>
+#include <ctime>
+#include <cmath>
+
+/*
+Projeden Ç?kard???m Dersler:
+1) Bir static s?n?f nesnesi e?er integral bir type ve const bir ifade ise bildirimde ilk de?er verebiliriz.
+Biraz kar???k bir ifade gelebilir bildirimde ilk de?er vermek. Bunu ?öyle yorumlamal?y?z. Bu static data member 
+class içinde hala bildirim olarak bulunuyor ama derleyicinin oyunu ile buna ilk de?er verebiliyoruz. 
+
+2) Static s?n?f nesnesi yukar?daki sart? sa?lam?yorsa ve s?n?f içerisinde ilk de?er vermek istiyorsak inline anahtar
+kelimesini kullan. ?nline variableler hayat?m?za C++17 ile girmi?tir.
+
+3) If a static data member of LiteralType is declared constexpr, it must be initialized with an initializer in which every expression
+is a constant expression, right inside the class definition.If a static data member of integral or enumeration type is declared 
+const (and not volatile), it can be initialized with an initializer in which every expression is a constant expression, right inside 
+the class definition:  https://en.cppreference.com/w/cpp/language/static
+
+
+
+*/
+
+#define isleap(year)	(((year % 4) ? 0 : ((year % 100) ? 1 : ((year % 400) ? 0 : 1))))
+
+enum class Weekday {
+	Sunday,
+	Monday,
+	Tuesday,
+	Wednesday,
+	Thursday,
+	Friday,
+	Saturday
+};
+
+
+namespace project {
+	class Date {
+	private:
+		inline static const char* dayStrings[] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+		inline static const char* monthStrings[] = {nullptr, "Ocak", "Subat", "Mart", "Nisan", "Mayis", "Haziran",
+														"Temmuz", "Agustos", "Eylul", "Ekim", "Kasim", "Aralik"};
+		inline static Weekday dayArray[7] = { Weekday::Sunday, Weekday::Monday, Weekday:: Tuesday, Weekday:: Wednesday, 
+													Weekday:: Thursday, Weekday:: Friday, Weekday::Saturday };
+		static constexpr int dayOfMonths[13] = { 0, 31,28,31,30,31,30,31,31,30,31,30,31 };
+		static constexpr int yearBase = 1922;  //1
+		int year = yearBase;
+		int month = 1;
+		int dayOfMonth = 1;
+		Weekday day = Weekday::Sunday;
+	public:
+		Date() = default;
+		Date(int d, int m, int y); //++
+		Date(const char* p); //7
+		Date(std::time_t timer); //8
+		int getMonthDay()const; //++
+		void printMonthDay()const; //++
+		int getMonth()const; //++
+		void printMonth()const; //++
+		int getYear()const; //++
+		void printYear() const; //++
+		int getYearDay()const; //++
+		void printYearDay()const; //++
+		Weekday getWeekDay()const; //++
+		void printWeekDay()const; //++
+		Date& setMonthDay(int day); //++
+		Date& setMonth(int month); //++
+		Date& setYear(int year); //++
+		Date& set(int day, int mon, int year); //++
+
+		Date operator-(int day)const; //++
+		Date& operator+=(int day); //++
+		Date& operator-=(int day); //++
+		Date& operator++(); //++
+		Date operator++(int); //++
+		Date& operator--(); //++
+		Date operator--(int); //++
+		static unsigned int sumOfDay(const Date&);
+		inline friend bool operator<(const Date& x, const Date& y) //++
+		{
+			if (x.year < y.year) {
+				return true;
+			}
+			else if (y.year < x.year) {
+				return false;
+			}
+			else if (x.month < y.month) {
+				return true;
+			}
+			else if (y.month < x.month) {
+				return false;
+			}
+			else if (x.dayOfMonth < y.dayOfMonth) {
+				return true;
+			}
+			return false;
+		}
+		friend bool operator==(const Date& x, const Date& y) //++
+		{
+			return ((x.year == y.year) && (x.month == y.month) && (x.dayOfMonth == y.dayOfMonth) ? true : false);
+		}
+
+		static constexpr bool isleap_(int y); //++
+
+		friend std::ostream& operator<<(std::ostream& scout, const Date& date) //++
+		{
+			return scout << date.dayOfMonth << " " << date.monthStrings[date.month] << " " << date.year << "\n";
+		}
+	};
+
+	bool operator<=(const Date&, const Date&); //++
+	bool operator>(const Date&, const Date&); //++
+	bool operator>=(const Date&, const Date&); //++
+	bool operator!=(const Date&, const Date&); //++
+
+	int operator-(const Date& d1, const Date& d2); //++
+	Date operator+(const Date& date, int n); //++
+	Date operator+(int n, const Date&); //++
+	Weekday& operator++(Weekday&); //30
+	Weekday operator++(Weekday&, int); //30
+	Weekday& operator--(Weekday&); //30
+	Weekday operator--(Weekday&, int); //30}
+}
+
+
+#endif DATE_H
